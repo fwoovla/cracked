e936567a8d97f47b5ac54ba24ac054b60aad9d13
fwoovla <91204566+fwoovla@users.noreply.github.com>
2025-07-20 20:06:08 -0400

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <raylib.h>
+
+int main(void) {
+   Vector2 newvec;
+   puts("Hello, world!");
+   return 0;
+}
---
+++ premake5.lua
@@ -0,0 +1,20 @@
+workspace "HelloWorld"
+   configurations { "Debug", "Release" }
+
+project "hello"
+   kind "ConsoleApp"
+   language "C++"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   files {
+          "**.h",
+          "**.cpp",
+         }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
