e9ad4befb8f94c9f25f992ae43f5fa96504e5308
fwoovla <rideoncleveland@gmail.com>
2025-07-21 09:36:04 -0400

    added raylib

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,14 @@
 #include <raylib.h>
 
 int main(void) {
+   InitWindow(300, 300, "cracked");
    Vector2 newvec;
    puts("Hello, you?");
+
+   while(true) {
+      BeginDrawing();
+      ClearBackground(RAYWHITE);
+      EndDrawing();
+   }
    return 0;
 }
