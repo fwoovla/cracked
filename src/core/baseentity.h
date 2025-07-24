badf733ab880be1b59e5a5dae6f83d565126d65e
fwoovla <rideoncleveland@gmail.com>
2025-07-24 11:48:39 -0400

    player ship added

---
+++ src/core/baseentity.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <raylib.h>
+#include "../globals.h"
+#include "../utils/utils.h"
+
+class BaseEntity  {
+    public:
+    virtual void Update(int *level_data) = 0;    
+};
+
+class DrawableEntity{
+    public:
+    virtual void Update(int *level_data) = 0;
+    virtual void Draw() = 0;
+    virtual bool CheckCollision(Vector4 &collision_data, int *level_array) = 0;
+};
